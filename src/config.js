a662dc3663fdb5a7b31fa5a8c411b0dc9084b4a0
Nathan Grasset <nathan.grasset@recast.ai>
2017-03-21 16:23:32 +0100

    update

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
- port: process.env.PORT || '5000',
- recast: {
-   token: process.env.REQUEST_TOKEN || '7be658c25cd312c99c24af00540f8697',
-   language: process.env.RECAST_LANGUAGE || 'en',
- },
+  port: process.env.PORT || '5000',
+  recast: {
+    token: process.env.RECAST_TOKEN || '42d8f54613bbc12eb818baf0e3b5c3a2',
+    language: process.env.RECAST_LANGUAGE || 'en',
+  },
 }
